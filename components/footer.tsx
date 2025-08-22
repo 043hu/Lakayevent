ee78a447cab3bd3ab6d487c22cae7cbb8274399f
Huawei <168386135+043hu@users.noreply.github.com>
2025-08-22 13:23:01 -0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,8 @@
 import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
 import React from "react";
 
-type Props = {};
 
-export default function Footer({}: Props) {
+export default function Footer() {
   return (
     <footer className='bg-gray-900 text-white pt-16 pb-8'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
